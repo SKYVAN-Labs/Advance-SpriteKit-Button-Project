82fe8754e6170ba1de84ead6bc7af9c0cea5d86d
Skyler Lauren <skylerlauren@Skylers-MacBook-Pro.local>
2017-09-10 10:25:28 -0500

    first commit

--- AdvanceSpriteKitButtonProject/GameScene.swift
+++ AdvanceSpriteKitButtonProject/GameScene.swift
@@ -9,81 +9,84 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SVLSpriteNodeButtonDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var leftArrowButton: SVLSpriteNodeButton!
+    var rightArrowButton: SVLSpriteNodeButton!
+    var shootButton: SVLSpriteNodeButton!
     
+    var ship: SKSpriteNode!
+
+    var lastUpdateTime: TimeInterval = 0
+    var shipSpeed: CGFloat = 10.0
+    
+    //MARK: - Scene Stuff
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        leftArrowButton = childNode(withName: "leftArrowButton") as! SVLSpriteNodeButton
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        rightArrowButton = childNode(withName: "rightArrowButton") as! SVLSpriteNodeButton
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        shootButton = childNode(withName: "shootButton") as! SVLSpriteNodeButton
+        shootButton.delegate = self
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        ship = childNode(withName: "ship") as! SKSpriteNode
+        shipSpeed = size.width/2.0
+
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
+        let dt = currentTime - lastUpdateTime
+        
+        if leftArrowButton.state == .down{
+            ship.position.x -= shipSpeed * CGFloat(dt)
+        }
+        
+        if ship.position.x < -size.width/2+ship.size.width/2{
+            ship.position.x = -size.width/2+ship.size.width/2
+        }
+        
+        if rightArrowButton.state == .down{
+            ship.position.x += shipSpeed * CGFloat(dt)
+        }
+        
+        if ship.position.x > size.width/2-ship.size.width/2{
+            ship.position.x = size.width/2-ship.size.width/2
+        }
+        
+        lastUpdateTime = currentTime
+    }
+    
+    func shoot(){
+        let bullet = SKSpriteNode(color: SKColor.red, size: CGSize(width: 10, height: 20))
+        bullet.position = ship.position
+        bullet.position.y += ship.size.height/2 + bullet.size.height/2
+        addChild(bullet)
+        
+        let moveUpAction = SKAction.moveBy(x: 0, y: size.height, duration: 1.0)
+        let destroy = SKAction.removeFromParent()
+        let sequence = SKAction.sequence([moveUpAction, destroy])
+        
+        bullet.run(sequence)
+    }
+    
+    //MARK: - SVLSpriteNodeButtonDelegate
+    func spriteButtonDown(_ button: SVLSpriteNodeButton){
+        print("spriteButtonDown")
+    }
+    
+    func spriteButtonUp(_ button: SVLSpriteNodeButton){
+        print("spriteButtonUp")
+    }
+    
+    func spriteButtonMoved(_ button: SVLSpriteNodeButton){
+        print("spriteButtonMoved")
+    }
+    
+    func spriteButtonTapped(_ button: SVLSpriteNodeButton){
+        if button == shootButton {
+            shoot()
+        }
     }
 }
